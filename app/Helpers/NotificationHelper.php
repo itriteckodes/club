fefb854341c6ce7fac851cfb94cfc082a4f616b6
itriteckodes <itriteckodes@gmail.com>
2021-09-04 08:29:46 +0500

    update

--- app/Helpers/NotificationHelper.php
+++ app/Helpers/NotificationHelper.php
@@ -18,7 +18,7 @@ class NotificationHelper
         $result = Http::withOptions(['json' => $notification])
             ->withHeaders(["authorization" => "key= AAAACD33jxg:APA91bF7oMcDQzQFOKjsJLLOvuBaXkyjZLp6H2UI6ydiSp1QANqwRmoSXEgn-BAuCienmcnrSr_84-3MOXBbtbbXOvRsVYbjsnqbmoUVCqVgE1Ljq98ZDWNCWgGtbZSpfx-QMl_nfUgY"])
             ->post("https://fcm.googleapis.com/fcm/send");
-            Log::alert($result);
+
     }
 
 }
